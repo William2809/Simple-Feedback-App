be37e58e107d57119e7b156e951e74c62da29186
William Huang <william.huang001@binus.ac.id>
2022-07-28 21:38:11 +0700

    commit

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/spinner.gif';
+
+function Spinner() {
+    return (
+        <div>
+            <img src={spinner} alt="loading..." style={{ width: '100px', margin: 'auto', display: 'block' }} />
+        </div>
+    )
+}
+
+export default Spinner
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: '1',
-            text: 'This item is from context1',
-            rating: 10,
-        },
-        {
-            id: '2',
-            text: 'This item is from context2',
-            rating: 10,
-        },
-        {
-            id: '3',
-            text: 'This item is from context3',
-            rating: 10,
-        },
-    ])
-
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
-    })
+    });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, [])
+
+    //fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc");
+        const data = await response.json();
+
+        setFeedback(data);
+        setIsLoading(false);
+    }
 
     // delete feedback
     const deleteFeedback = (id) => {
@@ -60,6 +57,7 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         editFeedback,
         updateFeedback,
+        isLoading,
     }}>
         {children}
     </FeedbackContext.Provider>
